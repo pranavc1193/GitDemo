7001a659f9aaeb2602622f54dcc43b4a017e8d46
pranavc1193 <pranavchaple99@gmail.com>
2021-09-16 12:55:14 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/seleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test
+	public void Browserauto ()
+	{
+	System.out.println("Test1");
+	}
+	
+	@Test
+	public void mobiletest()
+	{
+		System.out.println("Test2");
+	}
+}
+
+
